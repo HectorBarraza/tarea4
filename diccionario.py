3f2fac9cc693c7194660033039fef9a9ca98c801
“Cristobal <sick@MacBook-Pro-de-Hector.local>
2020-09-07 10:49:29 -0300

    diccionario.py

---
+++ diccionario.py
@@ -0,0 +1,11 @@
+diccionario = {'nombre': ['Hector','Grismi','Gabriela','Sebastian'], 'apellido': ['Barraza','Veliz','Gonzales', 'Perez'],'cursos': ['Programacion', 'fisica'], 'nota programacion': [40,70,42,22], 'nota fisica': [59,12,43,70]}
+
+print(diccionario['nota fisica'][0])
+print(diccionario['nombre'][0])
+print(diccionario['apellido'][0])
+print(diccionario['cursos'][0])
+print(diccionario['nota programacion'][0])
+print(diccionario['cursos'][1])
+print(diccionario['nota fisica'][0])
+
+
